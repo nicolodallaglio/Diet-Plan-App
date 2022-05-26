94bdb2251f89d98fe8dfb9121ce8a43fad846528
Nico <dallaglionicol@gmail.com>
2022-05-26 21:29:36 +0200

    1.1

--- startPage/src/main/java/com/example/startpage/Controller.java
+++ startPage/src/main/java/com/example/startpage/Controller.java
@@ -1,11 +1,21 @@
 package com.example.startpage;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
-
+import javafx.event.ActionEvent;
 public class Controller {
+    @FXML
+    private Button bottFirst;
+    @FXML
+    private Label labelFirst;
+    public void premiperproseguire(ActionEvent actionEvent){
+        bottFirst.setOpacity(0);
+        labelFirst.setOpacity(0);
+    }
     @FXML
     private TextField name;
     @FXML
@@ -17,8 +27,4 @@ public class Controller {
     @FXML
     private SplitMenuButton sporty;
 
-    public void chactData(){
-        name.getText();
-
-    }
 }
--- startPage/src/main/java/com/example/startpage/MainApplication.java
+++ startPage/src/main/java/com/example/startpage/MainApplication.java
@@ -2,19 +2,24 @@ package com.example.startpage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class MainApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+    public void start(Stage primarystage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("first-view.fxml"));
+
+        Controller controller = new Controller();
+
+        primarystage.setTitle("");
+        primarystage.setScene(new Scene(root));
+        primarystage.show();
     }
 
     public static void main(String[] args) {
