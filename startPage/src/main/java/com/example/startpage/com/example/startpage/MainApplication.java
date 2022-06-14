93d59d925d4b0c3ba90b428c3fe2144043fba66e
Nico <dallaglionicol@gmail.com>
2022-06-14 11:36:30 +0200

    1.2

--- startPage/src/main/java/com/example/startpage/com/example/startpage/MainApplication.java
+++ startPage/src/main/java/com/example/startpage/com/example/startpage/MainApplication.java
@@ -10,18 +10,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 public class MainApplication extends Application {
 
     public void start(Stage primarystage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("first-view.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("first-view.fxml")));
         primarystage.setTitle("");
         primarystage.setScene(new Scene(root));
         primarystage.show();
         primarystage.setResizable(false);
         primarystage.getIcons().add(
                 new Image(
-                        getClass().getResourceAsStream( "Mela.png" ))
+                        Objects.requireNonNull(getClass().getResourceAsStream("Mela.png")))
         );
     }
 
